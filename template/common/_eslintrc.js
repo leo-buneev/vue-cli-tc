5ff9d4cd7d1485939961a415c5df6b4bc2de07ef
Leo <leonid.buneev@tirecheck.cz>
2018-12-05 03:25:17 +0100

    Updated eslint config

--- template/common/_eslintrc.js
+++ template/common/_eslintrc.js
@@ -6,15 +6,9 @@ module.exports = {
   globals: {
     _: true,
   },
-  extends: ['plugin:vue/essential', 'plugin:tyrecheck/recommended', '@vue/prettier'],
+  extends: ['plugin:tyrecheck/recommended'],
   rules: {
-    'vue/require-prop-types': 0,
-    'vue/require-prop-type-constructor': 0,
-    'vue/require-default-prop': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': [process.env.NODE_ENV === 'production' ? 'error' : 'off', { allow: ['warn', 'error'] }],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
   },
 }
